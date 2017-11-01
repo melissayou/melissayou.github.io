4bfd2b13b056e59ddebe229eaa3659d17566ec79
Zengying You <zy41@cs.washington.edu>
2017-11-01 01:00:48 -0400

    final

--- project3/functions.js
+++ project3/functions.js
@@ -1,52 +1,52 @@
 function f1(x) {
-  return 5x-3x^2+13;
+  return (5*x-3*x*x+13);
 }
 
 function f2(a,b,c) {
-  var result = "Hello, " + a + " " + b + " " + c + "!";
+  let result = "Hello, " + a + " " + b + " " + c + "!";
   return result;
 }
 
 function f3(x,y) {
-  var fruit, color;
+  let fruit, color;
   if (x > y) {
-    fruit = "bananas",
-    color = "yellow"
+    fruit = "bananas";
+    color = "yellow";
   }
   else if (y > x) {
-    fruit = "porcupine",
-    color = "brown"
+    fruit = "porcupine";
+    color = "brown";
   }
   else if (x === y && x < 0 ) {
-    fruit = "red apples",
-    color = "red"
+    fruit = "red apples";
+    color = "red";
   } else {
-    fruit = "green apples",
-    color = "green"
+    fruit = "green apples";
+    color = "green";
   }
 
-content.innerHTML = fruit;
-content.style.color = color;
+  content.innerHTML = fruit;
+  content.style.color = color;
 }
 
 function f4(x) {
   var count = 0;
-  var numbers = [0,1,2,3,4,5,6,7,8,9];
-  for (int i = 0; i < x.length; i ++) {
+  var numbers = ['0','1','2','3','4','5','6','7','8','9'];
+  for (let i = 0; i < x.length; i++) {
     var char = x.charAt(i);
-    if (numbers.indexof(char) >= 0) {
+    if (numbers.indexOf(char) >= 0) {
       if (count === 0) {
         var firstChar = char;
       }
-      count ++;
+      count++;
     }
   }
 
-  if (((firstChar === '1') && count === 10) || count === 11) {
-    var ouuput = 'valid';
+  if (((firstChar === '1') && count === 11) || count === 10) {
+    var output = 'valid';
     var color = 'green';
   }else {
-    var ouuput = 'invalid';
+    var output = 'invalid';
     var color = 'red';
   }
   content.innerHTML = output;
@@ -54,36 +54,33 @@ function f4(x) {
 }
 
 function f5(x) {
-  var list = x.split(",");
+  let list = x.split(",");
   return list[2];
 }
 
 function f6() {
-  var buttons = document.getElementByTagName("button");
-  var length = buttons.length;
-  for (int i = 0; i < length; i++) {
-    var x = parseInt(this.id);
-    buttons[i].onclick = handleClick(x);
+  var buttons = document.getElementsByTagName("button");
+  let length = buttons.length;
+  for (let i = 0; i < length; i++) {
+    buttons[i].onclick = function(e) {
+      //reset all buttons
+      document.getElementById("1").style.backgroundColor = 'White';
+      document.getElementById("2").style.backgroundColor = 'White';
+      document.getElementById("3").style.backgroundColor = 'White';
+      document.getElementById("4").style.backgroundColor = 'White';
+      document.getElementById("5").style.backgroundColor = 'White';
+      let x = parseInt(this.id);
+      if (x <= 4) {
+        let next = (x + 1).toString();
+        document.getElementById(next).style.backgroundColor = "Blue";
+      }
+      if (x === 5) {
+        document.getElementById("1").style.backgroundColor = 'red';
+        document.getElementById("2").style.backgroundColor = 'red';
+        document.getElementById("3").style.backgroundColor = 'red';
+        document.getElementById("4").style.backgroundColor = 'red';
+        document.getElementById("5").style.backgroundColor = 'red';
+      }
+    }
   }
-}
-
-function handleClick(x) {
-  //reset all buttons
-  document.getElementById("1").style.backgroundColor = 'White';
-  document.getElementById("2").style.backgroundColor = 'White';
-  document.getElementById("3").style.backgroundColor = 'White';
-  document.getElementById("4").style.backgroundColor = 'White';
-  document.getElementById("5").style.backgroundColor = 'White';
-  if (x <= 4) {
-    var next = (x + 1).toString();
-    document.getElementById(next).style.backgroundColor = "Blue";
-  }
-  if (x === 5) {
-    document.getElementById("1").style.backgroundColor = 'red';
-    document.getElementById("2").style.backgroundColor = 'red';
-    document.getElementById("3").style.backgroundColor = 'red';
-    document.getElementById("4").style.backgroundColor = 'red';
-    document.getElementById("5").style.backgroundColor = 'red';
-  }
-
 }
